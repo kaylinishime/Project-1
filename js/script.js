cd252bd7036717da5bede8902c1846d49176a996
Kayli Nishime <kmnishime@ucdavis.edu>
2016-06-28 11:02:28 -0700

    drawing to board works

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
-var board = ['','','','','','','','','']
+var board = ['X','O','X','O','X','X','O','X','O']
 var turn = 'X'
+
+function drawBoard (){
+   var squares = document.querySelectorAll('.square');
+   for (var i=0; i<9; i++) {
+     squares[i].innerHTML = board[i];
+   }
+}
